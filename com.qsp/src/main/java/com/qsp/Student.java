f0f295395dc8e540cf5672c156d5c66718f99413
pavanwakade <pavanwakade199@gmail.com>
2025-02-11 21:56:46 +0530

    Spring

---
+++ com.qsp/src/main/java/com/qsp/Student.java
@@ -0,0 +1,14 @@
+package com.qsp;
+
+public class Student {
+	
+	public void study() {
+		
+		System.out.println("Do study hard");
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ com.qsp/src/main/java/com/qsp/StudentDriver.java
@@ -0,0 +1,61 @@
+package com.qsp;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class StudentDriver {
+
+	public static void main(String[] args) {
+		
+		Resource res=new ClassPathResource("Config.xml");
+		
+		BeanFactory bf=new XmlBeanFactory(res);
+		
+		Student student=bf.getBean("student", Student.class);
+		student.study();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
